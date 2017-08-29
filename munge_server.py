d29fb81b06962377757eb3a3c4cde9d7cefd5863
John Zulauf <jzulauf@yahoo.com>
2017-08-29 12:42:16 -0700

    Successfully completes quarter_list in 20m

--- munge_server.py
+++ munge_server.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python3
 
-
 from socketserver import ThreadingMixIn
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.request
 import io
 import sys
 from io import TextIOWrapper
+from functools import reduce
+import time
+
+base_time = 0
+def millitime():
+    return int(round(time.time() * 1000))
+
+def init_timer():
+    global base_time
+    base_time = millitime()
+
+def timestamp():
+    global base_time
+    return millitime() - base_time
 
 
 def process_url(url):
@@ -15,7 +28,14 @@ def process_url(url):
     digits = set(list("0123456789"))
     with urllib.request.urlopen(url) as f:
         last = ""
-        text = TextIOWrapper(f, encoding="utf-8")
+        #
+        # The spec states the contents of the urls will be UTF-8.
+        # Assume that all UTF-8 content has charset correctly set
+        # and that unlabeled content is the default per RFC
+        #
+        charset = f.headers.get_content_charset("ISO-8859-1") # get the charset if passed
+                                                              # or ISO-8859-1 (the official default)
+        text = TextIOWrapper(f, encoding=charset)
         for c in iter(lambda: text.read(1), '') :
             if c in digits or c == last:
                 continue
@@ -25,47 +45,120 @@ def process_url(url):
     return "".join(munged)
 
 debug_level = 2
+
+class MungedUrl:
+    def __init__(self, url):
+        try:
+            self.source = url.strip().rstrip()
+            self.result = process_url(self.source)
+        except UnicodeDecodeError as e:
+            self.success = False
+            self.result = ""
+            self.message = "FAILED:{}:{}, {}, at".format(self.source, e.encoding, e.reason, e.start)
+
+        except Exception as e:
+            self.success = False
+            self.result = ""
+            self.message = "FAILED:{}:{}".format(self.source, e.reason)
+        else:
+            self.success = True
+            self.message = "PASSED:{}".format(self.source)
+
+    def chomp(self):
+        if self.success:
+            self.result = self.result[:-1]
+    def first(self):
+        if self.success:
+            return self.result[0]
+        else:
+            return ""
+    def last(self):
+        if self.success:
+            return self.result[-1]
+        else:
+            return ""
+    def tidy(self, next_result):
+        last = self.last()
+        if last and last == next_result.first():
+            self.chomp()
+    def status(self):
+        return self.success
+    def length(self):
+        return len(self.response())
+    def response(self):
+        return self.result if self.status() else self.message
+
 class PostHandler(BaseHTTPRequestHandler):
+
+    def send_success_POST(self, munged):
+        # Filter out empty results
+        munged = [ m for m in munged if m.response() ]
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        # clean up any duplicates across result boundaries
+        if ( len(munged) ):
+            [ munged[i].tidy(munged[i+1]) for i in range(len(munged)-1) ]
+            length = sum([m.length() for m in munged])
+        else:
+            length = 0;
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        if ( len(munged) ):
+            self.wfile.write("".join([m.response() for m in munged]).encode("utf-8"))
+
+        return "OK"
+
+    def send_failed_POST(self, munged):
+        assert len(munged), "Reporting failure on empty request.  Empty is not a failure"
+        self.send_response(400) # Not sure this is a perfect response, but the client can detect failur
+        self.send_header("Content-Type", "text/plain; charset=utf-8")
+        response = "\n".join([m.response() for m in munged])
+        length = len(response)
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        self.wfile.write(response.encode("utf-8"))
+        if debug_level:
+            print("Failure Response:", response)
+
+        return "FAIL"
 
     def do_POST(self):
         global debug_level
         if debug_level:
-            print("bgn:do_POST:", self.requestline)
+            stamp = timestamp()
+            print("{}:BEGIN:do_POST:{}".format(stamp, self.requestline.strip().rstrip()))
             sys.stdout.flush()
+        else:
+            stamp = 0
 
         in_length = int(self.headers.get("Content-Length"))
         in_data = self.rfile.read(in_length)
         in_str  = in_data.decode('utf-8')
         if debug_level > 1:
             for u in in_str.split(","):
-                print("RECD:", u)
-                sys.stdout.flush()
+                print("{}:URL  :{}".format(stamp, u))
+            sys.stdout.flush()
 
 
         # Compute the response... with a chunk response capable
         # server this should be done chunkwise, but http.server isn't, apparently
-        # Would also be a place to parallelize on chunks (or urls, but I'm not
+        # Would also be a place to parallelize on chunks (or urls), but I'm not
         # clear on the thread safety of pool dispatch from within threads, and this
-        # isn't the time to test it)
-        url_list = [ u.strip().rstrip() for u in in_str.split(",")]
+        # isn't the time to test it
+        url_list = in_str.split(",")
         if url_list:
-            response = [ process_url(u) for u in url_list ]
+            munged = [ MungedUrl(u) for u in url_list ]
+            if all([m.status() for m in munged ]) :
+                code = self.send_success_POST(munged)
+            else:
+                code = self.send_failed_POST(munged)
             
-            length = len(response[0])
-            for i in range(1, len(response)):
-                length += len(response[i])
-                if response[i] and response[i-1].endswith(response[i][0]) :
-                    response[i-1] = response[i-1][:-1]
-                    length -= 1
-
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        self.wfile.write("".join(response).encode("utf-8"))
+        else:
+            send_failed_POST([])
+            code = "NOOP"
 
         if debug_level > 1:
-            print("end:do_POST:", self.requestline)
+            print("{}:{: >5}:do_POST:".format(stamp, code))
             sys.stdout.flush()
 
 class ThreadedServer(ThreadingMixIn, HTTPServer):
@@ -80,6 +173,7 @@ def main():
     # Needs to be parameterized, especially vs. ThreadedServer below
     #CreateGlobalPool(2) # Allow each concurrent handler up to two workers.
     server =  ThreadedServer(('localhost', 8675), PostHandler)
+    init_timer()
     server.serve_forever()
 
 main()
--- post_pusher.py
+++ post_pusher.py
@@ -1,4 +1,22 @@
 #! /usr/bin/env python3
+"""
+post_pusher.py
+
+test and exerciser for the http app server munge_server.py
+
+Given the name of a server containing files to fetch and munge
+randomly group the files together in munge requests POST'd to
+the munge_server, and optionally testing that the file munging
+meets the munge criteria -- which is super secret, so like
+don't read the code to figure it out, okay, really.
+
+Note the the validation is incomplete in that it only tests
+if the munge criteria is met, but does not independently verify
+that the munging wasn't carried our in excess of the criteria.
+
+For completeness this should be added.
+"""
+
 
 
 from multiprocessing import Pool
@@ -8,7 +26,11 @@ import urllib.request
 import random
 import sys
 
+# Globals
+debug_level = 0
+
 def CreatePool(num_threads=0) :
+    """The pool allows for multiple post to be sent simulataneously"""
     if num_threads > 0:
         pool = Pool(num_threads)
     else:
@@ -17,6 +39,7 @@ def CreatePool(num_threads=0) :
     return pool
 
 def test_response(response) : 
+    """Read the response and verify that the content meets the munging criteria"""
     last = ""
     offset = 1
     digits = set(list("0123456789"))
@@ -24,28 +47,44 @@ def test_response(response) :
     for c in iter(lambda: response.read(1), '') :
         out.append(c)
         if c in digits:
-            print(":".join(out))
+            if debug_level > 1:
+                print(":".join(out))
             return "FAIL: digit at {}:".format(offset)
         elif c == last:
-            print(":".join(out))
+            if debug_level > 1:
+                print(":".join(out))
             return "FAIL: dupl. at {}:".format(offset)
         offset += 1
         last = c
     return "PASS"
       
 class Poster:
-    def __init__(self, post_url):
+    def __init__(self, post_url, validation_level):
         self.post_url = post_url
+        self.do_test = validation_level > 0
 
     def __call__(self, message):
-        print("Sending", message)
-        sys.stdout.flush()
-        payload = message.encode("utf-8")
-        result =  "FAIL:urlopen: {}".format(message)
-        with urllib.request.urlopen(self.post_url, payload) as f:
-            t = TextIOWrapper(f, encoding="utf-8")
-            result =  "{}: {}".format(test_response(t), message)
+        call_result =  "FAIL:urlopen"
+        try:
+            if debug_level > 2:
+                print("Sending", message)
+                sys.stdout.flush()
+            payload = message.encode("utf-8")
+            with urllib.request.urlopen(self.post_url, payload) as f:
+                if f.status == 200 :
+                    t = TextIOWrapper(f, encoding="utf-8")
+                    if self.do_test:
+                        call_result = test_response(t)
+                    else:
+                        call_result = "PASS:trivial"
+                else:
+                    call_result = "FAIL:server_err"
+        except urllib.error.URLError as e:
+            call_result = "FAIL:HTTPError:{}:{}".format(e.reason, e.code)
+        except:
+            call_result = "FAIL:unknown:" + sys.exc_info()[0]
 
+        result =  "{}: {}".format(call_result, message)
         return result
         
 def create_grouped_urls(file_server, files):
@@ -54,48 +93,62 @@ def create_grouped_urls(file_server, files):
     current_group = []
     for f in files:
         url = file_server + "/" + f.rstrip().strip()
-        print("Url:", url)
+        if debug_level > 1:
+            print("Url:", url)
         if random.randint(0,2) :
             # Two thirds of the time we'll add a url to the current group
             # to get a pseudo random variation in the number of urls
             current_group.append(url)
         else:
             groups.append(", ".join(current_group))
+            if debug_level > 0:
+                print("Group:", groups[-1])
             current_group = [ url ]
     if current_group:
-        print("Group:", groups[-1])
         groups.append(", ".join(current_group))
+        if debug_level > 0:
+            print("Group:", groups[-1])
 
-    #print("Groups", groups)
     return groups
         
 
-def main(post_url, file_server, file_list, num_threads):
-    files = open(file_list,"r")
-    groups = create_grouped_urls(file_server, files)
+def main(post_url, file_server, file_list, num_threads, one_group, validation_level):
+    if not one_group:
+        files = open(file_list,"r")
+        groups = create_grouped_urls(file_server, files)
+    else:
+        groups = [ one_group ]
+    if debug_level > 3:
+        print("Groups:", groups)
     pool = CreatePool(num_threads)
-    # Lambda really should work here....
+    # Lambda really should work here.... but map wants to pickle the proc
+    # and you can't pickle lambda's apparently
     #poster = Poster(post_url)
     #post = lambda g: poster.post(g)
-    results = pool.map(Poster(post_url), groups)
+    results = pool.map(Poster(post_url,validation_level), groups)
     print("\n".join(results))
 
 
 def usage():
-    print("Usage: {} <opts> file_list", sys.argv[0])
+    print("Usage:")
+    print("    {} <opts> file_list".format(sys.argv[0]))
+    print("    {} -u url_list".format(sys.argv[0]))
     print("\nOptions")
     print("    -f file_server    the URL of the root of the file_list")
     print("    -h app_server     the URL file munging app server")
     print("    -w num_threads    the paralell width for post creation")
+    print("    -u url_list       post one request with the csv url_list given")
+    print("    -v validation_lvl control the level of testing > 0 for munge testing (default 1)")
 
 import getopt
 app_host = "http://localhost:8675"
 file_server = "http://localhost"
 width = 4
-opts, args = getopt.getopt(sys.argv[1:], "f:h:w:")
-file_list = args[0]
-if len(args) != 1 :
-    raise Exception('missing url file arg, or too many')
+opts, args = getopt.getopt(sys.argv[1:], "f:h:w:u:v:")
+one_group = ""
+ok_args = 1
+validation_level = 1
+file_list = ""
 
 for o, a in opts:
     if o == "-h":
@@ -104,7 +157,22 @@ for o, a in opts:
         width = int(a)
     elif o == "-f":
         file_server = a
+    elif o == "-v":
+        validation_level = int(a)
+    elif o == "-u":
+        # Debug flag to force the pusher to send this exact group of URLS
+        one_group = a
+        width = 1
+        ok_args = 0
     else:
+        usage()
         raise Exception('unknown flag')
 
-main(app_host, file_server, file_list, width)
+if len(args) != ok_args :
+    usage()
+    raise Exception("saw {} args, expected {}".format(len(args), ok_args))
+
+if ok_args == 1:
+    file_list = args[0]
+
+main(app_host, file_server, file_list, width, one_group, validation_level)
