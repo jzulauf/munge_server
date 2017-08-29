a3237516b87000d22da1b39d6a933fe4ff6ff395
John Zulauf <jzulauf@yahoo.com>
2017-08-29 07:02:40 -0700

    Working munge_server without error checking

--- munge_server.py
+++ munge_server.py
@@ -6,27 +6,67 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.request
 import io
 import sys
+from io import TextIOWrapper
 
+
+def process_url(url):
+    url= url.rstrip()
+    munged = []
+    digits = set(list("0123456789"))
+    with urllib.request.urlopen(url) as f:
+        last = ""
+        text = TextIOWrapper(f, encoding="utf-8")
+        for c in iter(lambda: text.read(1), '') :
+            if c in digits or c == last:
+                continue
+            last = c
+            munged.append(c)
+      
+    return "".join(munged)
+
+debug_level = 2
 class PostHandler(BaseHTTPRequestHandler):
+
     def do_POST(self):
-        print("bgn:do_POST:", self.requestline)
-        sys.stdout.flush()
+        global debug_level
+        if debug_level:
+            print("bgn:do_POST:", self.requestline)
+            sys.stdout.flush()
 
         in_length = int(self.headers.get("Content-Length"))
         in_data = self.rfile.read(in_length)
         in_str  = in_data.decode('utf-8')
-        for u in in_str.split(","):
-            print("RECD:", u)
-            sys.stdout.flush()
-        self.send_response(200)
-        response = "quack"
-        self.send_header("Content-Type", "text/plain")
-        self.send_header("Content-Length", str(len(response)))
-        self.end_headers()
-        self.wfile.write(response.encode("utf-8"))
+        if debug_level > 1:
+            for u in in_str.split(","):
+                print("RECD:", u)
+                sys.stdout.flush()
 
-        print("end:do_POST:", self.requestline)
-        sys.stdout.flush()
+
+        # Compute the response... with a chunk response capable
+        # server this should be done chunkwise, but http.server isn't, apparently
+        # Would also be a place to parallelize on chunks (or urls, but I'm not
+        # clear on the thread safety of pool dispatch from within threads, and this
+        # isn't the time to test it)
+        url_list = [ u.strip().rstrip() for u in in_str.split(",")]
+        if url_list:
+            response = [ process_url(u) for u in url_list ]
+            
+            length = len(response[0])
+            for i in range(1, len(response)):
+                length += len(response[i])
+                if response[i] and response[i-1].endswith(response[i][0]) :
+                    response[i-1] = response[i-1][:-1]
+                    length -= 1
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", str(length))
+        self.end_headers()
+        self.wfile.write("".join(response).encode("utf-8"))
+
+        if debug_level > 1:
+            print("end:do_POST:", self.requestline)
+            sys.stdout.flush()
 
 class ThreadedServer(ThreadingMixIn, HTTPServer):
     """Using the SocketServer mixin to make the HTTPServer multithreaded"""
@@ -34,17 +74,11 @@ class ThreadedServer(ThreadingMixIn, HTTPServer):
 
 
 
-def process_url(url):
-    url= url.rstrip()
-    count = -1
-    with urllib.request.urlopen(url) as f:
-        count = 0
-        for l in f:
-            count += 1
-    return "{}: {}".format(url, count)
-        
 
 def main():
+    # Not sure if the pool object is thread safe... disable for now
+    # Needs to be parameterized, especially vs. ThreadedServer below
+    #CreateGlobalPool(2) # Allow each concurrent handler up to two workers.
     server =  ThreadedServer(('localhost', 8675), PostHandler)
     server.serve_forever()
 
--- post_pusher.py
+++ post_pusher.py
@@ -2,8 +2,11 @@
 
 
 from multiprocessing import Pool
+from io import TextIOWrapper
 
 import urllib.request
+import random
+import sys
 
 def CreatePool(num_threads=0) :
     if num_threads > 0:
@@ -13,21 +16,81 @@ def CreatePool(num_threads=0) :
 
     return pool
 
-def count(url):
-    url= url.rstrip()
-    count = -1
-    with urllib.request.urlopen(url) as f:
-        count = 0
-        for l in f:
-            count += 1
-    return "{}: {}".format(url, count)
+def test_response(response) : 
+    last = ""
+    offset = 1
+    digits = set(list("0123456789"))
+    out = []
+    for c in iter(lambda: response.read(1), '') :
+        out.append(c)
+        if c in digits:
+            print(":".join(out))
+            return "FAIL: digit at {}:".format(offset)
+        elif c == last:
+            print(":".join(out))
+            return "FAIL: dupl. at {}:".format(offset)
+        offset += 1
+        last = c
+    return "PASS"
+      
+class Poster:
+    def __init__(self, post_url):
+        self.post_url = post_url
+
+    def __call__(self, message):
+        print("Sending", message)
+        sys.stdout.flush()
+        payload = message.encode("utf-8")
+        result =  "FAIL:urlopen: {}".format(message)
+        with urllib.request.urlopen(self.post_url, payload) as f:
+            t = TextIOWrapper(f, encoding="utf-8")
+            result =  "{}: {}".format(test_response(t), message)
+
+        return result
+        
+def group_urls(url_file):
+    random.seed(42,2) # For consistant results
+    groups = []
+    current_group = []
+    for url in url_file:
+        url = url.rstrip()
+        print("Url:", url)
+        if random.randint(0,2) :
+            current_group.append(url)
+        else:
+            groups.append(", ".join(current_group))
+            current_group = [ url ]
+    if current_group:
+        groups.append(", ".join(current_group))
+
+    print("Groups", groups)
+    return groups
         
 
-def main(url_file, num_threads):
+def main(post_url, url_file, num_threads):
     urls = open(url_file,"r")
+    groups = group_urls(urls)
     pool = CreatePool(num_threads)
-    for r in pool.map(count, urls):
-        print(r)
+    # Lambda really should work here....
+    #poster = Poster(post_url)
+    #post = lambda g: poster.post(g)
+    results = pool.map(Poster(post_url), groups)
+    print("\n".join(results))
 
 
-main("medium_url",4)
+import getopt
+host = "http://localhost:8675"
+width = 4
+opts, args = getopt.getopt(sys.argv[1:], "h:w:")
+url_file = args[0]
+if len(args) != 1 :
+    raise Exception('missing url file arg, or too many')
+for o, a in opts:
+    if o == "-h":
+        host = a
+    elif o == "-w":
+        width = int(a)
+    else:
+        raise Exception('unknown flag')
+
+main(host, url_file, width)
